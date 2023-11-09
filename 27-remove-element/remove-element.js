e1f4b3dc5e0b5a8cca5e244b202629adc2b7da7a
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2023-11-09 14:27:57 -0500

    Time: 49 ms (73.20%) | Memory: 41.9 MB (61.77%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+ /*
+ P: Given an array of numbers and a number value remove all occurences where the value appears in the number array in-place. Then return the number od elements in the nums array that are not equal to val.
+
+
+ E:
+ D:
+ A:
+ C:
+ */
+var removeElement = function(nums, val) {
+    let pointer = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== val){
+            nums[pointer]= nums[i]
+            pointer++
+        }
+    }
+    return pointer
+};
