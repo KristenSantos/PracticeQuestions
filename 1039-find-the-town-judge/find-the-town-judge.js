14da9fe53b9a365d09cb5f8559cf4a875c6f8537
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2023-11-10 19:02:15 -0500

    Time: 91 ms (49.17%) | Memory: 49.7 MB (96.96%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.js
+++ 1039-find-the-town-judge/find-the-town-judge.js
@@ -20,18 +20,23 @@
 //         return -1
 //     }
 // };
+/*
+Input: n = 3, trust = [[1,3],[2,3]]
+trustScores = [0,-1,-1,2]
+
+*/
 
 const findJudge = (n, trust) => {
-    const trustScores = new Array(n + 1).fill(0)
+let trustArray = new Array(n+1).fill(0);
+for(const [a,b] of trust){
+    trustArray[a]--
+    trustArray[b]++;
+}
+for(let i = 1; i < trustArray.length; i++){
+    if(trustArray[i] === n-1){
+        return i
+    }
+}
+return -1
 
-    for(const [a, b] of trust){
-        trustScores[a]--
-        trustScores[b]++
-    }
-    for(let i = 1; i < trustScores.length; i++){
-        if(trustScores[i] === n - 1){
-            return i 
-        } 
-    }
-    return -1   
 };
