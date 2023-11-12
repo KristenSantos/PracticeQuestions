8e18ca739262cb19eaff2e4a75ac0a8d06986f28
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2023-11-12 16:23:03 -0500

    Time: 61 ms (70.72%) | Memory: 44.8 MB (71.65%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+ /*
+ P: Find if the linked list is a cycle
+ E:
+
+ */
+var hasCycle = function(head) {
+    let fast = head;
+    let slow = head;
+    while(fast !== null && fast.next !== null){
+        slow = slow.next;
+        fast = fast.next.next
+        if (fast === slow){
+            return slow
+        }
+    }
+    return false
+
+};
