18fed864144e2c4ca5c48a7b2f48713d5d3f1a0b
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2023-11-15 17:21:21 -0500

    Time: 78 ms (8.16%) | Memory: 43.5 MB (76.45%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -5,10 +5,21 @@
  */
 
  /*
- P:
- E:
- D:
- A:
+ P: Given two arrays the first array being a subset of the second array
+ if the number has a next greater element
+
+ E:  input: nums1:[9,8,2], nums2:[2,6,8,1,7,9]
+     output: [-1, 9, 6]
+ D: Array
+ A: 
+ Start by intializing a new array this is the array that we will be returning at the end
+ We want to iterate through the first array to get the element to compare in the second array
+ Next we want to initalize a new variable this is what will be the next greater element so we set this to -1 and reassign if we find a number larger than i 
+ Following that we want to create a for loop that iterates backward through nums2 unitil it finds the element i from nums 1 
+ next we want to check and see if that number in nums2 is greater than i and if it is reasign x to be nums2[j]
+ following that we should push x into the array and countinue throught the nums1 array
+ return the array at the end
+
  C:
  */
 
