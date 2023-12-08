0434699094a806bd47248f760a6cfad8da9f1e17
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2023-12-08 13:14:58 -0500

    Time: 151 ms (53.81%) | Memory: 51.1 MB (59.98%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.js
@@ -0,0 +1,62 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+ /*
+ */
+
+/*
+P: repace every element with the greatest element to the right of that element
+
+E: input: arr = [8,9,2,9,1,2,5,6]
+output: [9, 9, 9, 6, 6, 6, 6, -1]
+D:
+A:
+C:
+*/
+ 
+var replaceElements = function(arr) {
+    let max = arr[arr.length - 1];
+    arr[arr.length-1] = -1;
+    for(let i = arr.length-2; i >= 0; i--){
+        if(max > arr[i]){
+            arr[i] = max
+        }
+        else {
+            const hold = max;
+            max = arr[i]
+            arr[i] = hold
+        }
+    }
+    return arr
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   let max = arr[arr.length-1];
+//   arr[arr.length-1] = -1;
+//   for(let i= arr.length-2; i >= 0; i--){
+//       if (max > arr[i]){
+//           arr[i] = max
+//       } else{
+//           const hold = max;
+//           max = arr[i];
+//           arr[i] = hold
+//       }
+//   }
+// return arr
