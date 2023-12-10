3f756e63b877196637a66767891c5429947737b9
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2023-12-10 18:05:20 -0500

    Time: 58 ms (84.71%) | Memory: 44.6 MB (41.88%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.js
+++ 724-find-pivot-index/find-pivot-index.js
@@ -10,18 +10,18 @@
  C:
  */
 var pivotIndex = function(nums) {
-    let totalSum = 0;
-
+    let total = 0;
+    let leftSum = 0 
     for(let i = 0; i < nums.length; i++){
-        totalSum += nums[i]
+        total += nums[i]
     }
-
-     let leftSum = 0;
-     for(let i = 0; i < nums.length; i++){
-         let rightSum = totalSum - leftSum - nums[i]
-         if(rightSum === leftSum) return i
-
-         leftSum += nums[i]
-     }
+    for(let i = 0; i < nums.length; i++){
+        let rightSum = total - leftSum - nums[i]
+       
+        if(leftSum === rightSum){
+            return i
+        }
+        leftSum += nums[i]
+    }
     return -1
 };
