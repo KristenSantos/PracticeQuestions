89afe9f347831b4f6b2043ff0300e7a1459a75f4
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2023-12-10 16:16:30 -0500

    Time: 184 ms (5.05%) | Memory: 51 MB (5.05%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+ /*
+ 1. Create empty set
+ 2. Loop through emails
+    a. Create Variable Local and Domain from split at "a"
+    b. Delete "." from local 
+    c. Split local from "+"
+    d. Create unique email variable to hold local + "a" + domain
+    e. add unique email to set
+3. Return the size of the set
+ */
+var numUniqueEmails = function(emails) {
+    const emailSet = new Set();
+    for(let i = 0; i < emails.length; i++){
+        let [local, domain] = emails[i].split('@')
+        local = local.replaceAll('.', "")
+        local = local.split('+')
+        console.log(local)
+        let newDomain = local[0] + '@' + domain
+        emailSet.add(newDomain)
+    }
+    console.log(emailSet)
+    return emailSet.size
+};
