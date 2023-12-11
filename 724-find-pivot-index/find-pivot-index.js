9754acc61e036a8eb2dc5cbc3228a33abc9af1b0
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2023-12-11 12:22:17 -0500

    Time: 75 ms (26.74%) | Memory: 44.1 MB (86.31%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.js
+++ 724-find-pivot-index/find-pivot-index.js
@@ -10,18 +10,52 @@
  C:
  */
 var pivotIndex = function(nums) {
-    let total = 0;
-    let leftSum = 0 
+    const sum = nums.reduce((accumulator, currentVal) => {
+        return accumulator + currentVal
+    }, 0 )
+    let leftTotal = 0;
     for(let i = 0; i < nums.length; i++){
-        total += nums[i]
-    }
-    for(let i = 0; i < nums.length; i++){
-        let rightSum = total - leftSum - nums[i]
-       
-        if(leftSum === rightSum){
+         const rightTotal = sum - leftTotal - nums[i]
+        if(leftTotal === rightTotal){
             return i
         }
-        leftSum += nums[i]
+        leftTotal+= nums[i]
     }
     return -1
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let total = 0;
+    // let leftSum = 0 
+    // for(let i = 0; i < nums.length; i++){
+    //     total += nums[i]
+    // }
+    // for(let i = 0; i < nums.length; i++){
+    //     let rightSum = total - leftSum - nums[i]
+       
+    //     if(leftSum === rightSum){
+    //         return i
+    //     }
+    //     leftSum += nums[i]
+    // }
+    // return -1
