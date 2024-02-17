accf65f52b5b647e9404897e1d610a792708dac7
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2024-02-17 13:12:47 -0500

    Time: 69 ms (15.74%) | Memory: 52.4 MB (5.37%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let newString = '';
+    s = s.split(" ")
+    console.log(s)
+    for(let i = s.length-1; i >= 0; i--){
+        if(s[i] !== ''){
+         newString += ` ${s[i]}`
+        }  
+    }
+    return newString.trim()
+};
