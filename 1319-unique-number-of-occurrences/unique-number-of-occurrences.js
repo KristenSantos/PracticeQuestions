b8a699f23d5e4b6acdb2e3e2e920e999dc08a3f1
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2024-02-20 13:24:27 -0500

    Time: 59 ms (32.69%) | Memory: 50.3 MB (9.89%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+ /*
+ P:
+ E:
+ D:
+ A:
+ C:
+ */
+var uniqueOccurrences = function(arr) {
+    let occurenceMap = new Map();
+    let reoccurenceSet = new Set()
+    for(let num of arr){
+        if(occurenceMap.has(num)){
+            occurenceMap.set(num, occurenceMap.get(num)+1)
+        } else{
+            occurenceMap.set(num, 1)
+        }
+    }
+    for (let values of occurenceMap.values()) {
+    if(!reoccurenceSet.has(values)){
+        reoccurenceSet.add(values)
+    } else {
+        return false
+    }
+};
+console.log(reoccurenceSet)
+    return true
+};
