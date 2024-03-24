c90bf3740ec011de741a074c2ddf5463e3cea796
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2024-03-24 15:14:09 -0400

    Time: 107 ms (6.14%) | Memory: 53.7 MB (67.05%) - LeetSync

---
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    nums.sort((a,b) => a-b);
+    let max1 = nums[nums.length-1] * nums[nums.length-2] * nums[nums.length-3];
+    let max2 = nums[nums.length-1] * nums[0] * nums[1];
+
+    return max1 > max2 ? max1 : max2
+};
