2e34b6bb53045b9f4d479f485a634d6f3f53e56c
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2024-04-15 14:59:09 -0400

    Time: 58 ms (16.44%) | Memory: 48.4 MB (87.44%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.js
@@ -0,0 +1,100 @@
+// class MyQueue {
+//     constructor() {
+//         this.stack1 = [];
+//         this.stack2 = [];
+//     }
+
+//     push(x) {
+//         this.stack1.push(x);
+//     }
+
+//     pop() {
+//         if(this.stack2.length === 0){
+//             this.transferStack1()
+//         }
+
+//         return this.stack2.pop();
+//     }
+
+//     peek() {
+//         if(this.stack2.length === 0 ){
+//             this.transferStack1()
+//         }
+
+//         return this.stack2[this.stack2.length-1]
+//     }
+
+//     empty() {
+//         return this.stack1.length === 0 && this.stack2.length === 0;
+//     }
+
+//     transferStack1() {
+//         while(this.stack1.length > 0){
+//             this.stack2.push(this.stack1.pop());
+//         }
+//     }
+// }
+
+
+
+
+
+
+var MyQueue = function(){
+    this.stack1 = [];
+    this.stack2 = [];
+}
+
+
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+        if(this.stack2.length === 0){
+            this.transferStack1()
+        }
+
+        return this.stack2.pop();  
+};
+
+/**
+ * @return {number}
+ */
+
+MyQueue.prototype.peek = function() {
+        if(this.stack2.length === 0 ){
+            this.transferStack1()
+        }
+        return this.stack2[this.stack2.length-1]
+};
+
+MyQueue.prototype.transferStack1 = function() {
+    while(this.stack1.length > 0){
+             this.stack2.push(this.stack1.pop());
+}
+}
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack1.length === 0 && this.stack2.length === 0;
+    
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
