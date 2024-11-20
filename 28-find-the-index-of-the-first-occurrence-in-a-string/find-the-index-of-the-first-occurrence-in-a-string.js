51427c8bef44f0d8c9dc71473f4bdf164f0621d4
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2024-11-20 17:53:13 -0500

    Time: 0 ms (100.00%) | Memory: 48.5 MB (87.75%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+/*
+P:
+E:
+D: 
+A: 
+C:
+*/
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+    
+};
