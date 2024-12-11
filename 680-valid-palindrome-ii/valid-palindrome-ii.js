eff288e9baf9ae7aff20470dd63149516b1caa0d
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2024-12-10 22:45:31 -0500

    Time: 9 ms (51.59%) | Memory: 57.9 MB (22.08%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.js
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -3,22 +3,32 @@
  * @return {boolean}
  */
 var validPalindrome = function(s) {
-    let left = 0;
-    let right = s.length-1
-
+    let left = 0
+    let right = s.length - 1;
     while(left < right){
-        if(s[left] !== s[right]){
-            let skipLeft = s.substring(left+1, right+1);
-            let skipRight = s.substring(left, right);
-
-            if(skipLeft === skipLeft.split("").reverse().join("") || skipRight === skipRight.split("").reverse().join("")){
-                return true
-            } else {
-                return false
-            }
+        if(s[right] !== s[left]){
+            let skipLeft = s.slice(left+1, right+1);
+            let skipRight = s.slice(left, right);
+            return isPalindrome(skipLeft) || isPalindrome(skipRight)
         }
+        left++;
         right--
-        left++
+
+    }
+
+    return true
+
+};
+
+function isPalindrome(str){
+    let left = 0; 
+    let right  = str.length -1; 
+    while(left < right){
+        if(str[left] !== str[right]){
+            return false
+        }
+        left++;
+        right--;
     }
     return true
-};
+}
