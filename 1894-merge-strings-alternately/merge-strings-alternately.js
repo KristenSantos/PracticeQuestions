2ef701b52064670dc190531eeea8e836d733105a
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2024-12-17 17:39:19 -0500

    Time: 66 ms (6.21%) | Memory: 50.8 MB (8.24%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -4,15 +4,18 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    const largest = Math.max(word1.length, word2.length)
-    let mergedString = ""
-    for(let i = 0; i < largest; i++){
-        if(word1[i]){
-            mergedString += word1[i]
-        } 
-        if(word2[i]){
-             mergedString += word2[i]
+    let mergedString = '';
+    let len = word1.length > word2.length ? word1.length : word2.length
+        for(let i = 0; i < len; i++){
+            if(word1[i] === undefined){
+                 mergedString += word2[i]
+            } else if(word2[i] === undefined) {
+                 mergedString += word1[i]
+            } else {
+                mergedString += word1[i];
+                mergedString += word2[i];
+            }
+
         }
-    }
-    return mergedString
+        return mergedString
 };
