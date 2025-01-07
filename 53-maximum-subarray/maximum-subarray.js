f7e9a6b58153d7b0b5cb910bb706d37e4157cbe2
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2025-01-06 20:19:23 -0500

    Time: 1 ms (85.39%) | Memory: 58.4 MB (43.30%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -3,33 +3,16 @@
  * @return {number}
  */
  /*
- P:
- we're given an array of numbers
- return the largest sum from a sub array
- D: two pointer
 
- [-3,4,-1,2,1,-5,4]
-
-
- A: 
- Create a variable greatest sum; 
-
- 
  */
 var maxSubArray = function(nums) {
-
-let currSum = 0 
-let maxSum = nums[0];
-if (nums.length === 1)return nums[0]
-for(let i = 0; i < nums.length; i++){
-    currSum += nums[i];
-
-    if(maxSum < currSum){
-        maxSum = currSum
+    let maximumValue = nums[0];
+    let currentRunningSum = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(currentRunningSum < 0) currentRunningSum = 0;
+        currentRunningSum += nums[i];
+        maximumValue = Math.max(maximumValue, currentRunningSum)
     }
-    if( currSum < 0 ){
-        currSum = 0; 
-    } 
-}
-    return maxSum
+    return maximumValue
+
 };
