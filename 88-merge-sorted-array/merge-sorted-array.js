b5d2681a971f3d8e236f22172ca8c78523a0fef2
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2025-01-12 19:34:16 -0500

    Time: 0 ms (100.00%) | Memory: 49 MB (64.96%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -8,17 +8,29 @@
 /*
 P: Merge two non-decreasing ordered array
 E:
-D:
+D: 
 A:
 C:
 */
 var merge = function(nums1, m, nums2, n) {
-    nums1.splice(m, nums1.length - m)
-    nums2.splice(m, nums1.length - m)
-    let i = m+n-1
-    m--
-    n--
-    while(n >= 0){
-        nums1[i--] = (m >= 0 && nums1[m] > nums2[n]) ? nums1[m--] : nums2[n--]
+    let last = m+n-1;
+
+    while(m > 0 && n > 0 ){
+        if(nums1[m - 1] > nums2[n-1]){
+            nums1[last] = nums1[m-1];
+            m--
+        }else {
+            nums1[last] = nums2[n-1]
+            n--
+        }
+        last--
+
     }
+    while(n > 0){
+        nums1[last] = nums2[n-1];
+        n = n-1
+        last = last - 1
+    }
+
+
 };
