0888f506a7ed03b162f1736e229b348bd4e777a1
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2025-01-26 10:28:15 -0500

    Time: 952 ms (5.03%) | Memory: 70.3 MB (5.45%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,40 @@
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = (height) => {
+    let max = -Infinity;
+    let s = 0;
+    let e = height.length-1;
+    while(s <= e){
+        let currMin = height[s] < height[e] ? height[s] : height[e];
+        let currWidth = e-s;
+        let currArea = currMin * currWidth;
+        console.log(currArea)
+        max = Math.max(max, currArea)
+        if(height[s] < height[e]){
+            s++
+        } else {
+            e--
+        }
+    }
+    console.log(max)
+    return max
+}
+
+
+
+// Nested for loop but still works when not given a big number
+// var maxArea = function(height) {
+//     let max = 0; 
+//     for(let i = 0; i < height.length; i++){
+//         let currentHeight = height[i]
+//         for(let j = 0; j <  height.length; j++){
+//             let minHeight = currentHeight < height[j] ? currentHeight : height[j]
+//             let width = j - i;
+//             max = Math.max(max, minHeight * width)
+//         }
+//     }
+//     return max
+// };
