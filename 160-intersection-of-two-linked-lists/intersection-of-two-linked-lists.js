d6349b6213efc51107f73224f2fd2738b2caf18c
Kristen Santos <90364347+KristenSantos@users.noreply.github.com>
2025-02-04 17:59:45 -0500

    Time: 84 ms (39.72%) | Memory: 58.9 MB (42.60%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -17,35 +17,32 @@ var getIntersectionNode = function(headA, headB) {
     let linkedListA = headA;
     let linkedListB = headB;
 
-    // Calculate lengths of both lists
-    while (linkedListA) {
+    while(linkedListA){
         linkedListA = linkedListA.next;
-        lengthA++;
-    }
-    while (linkedListB) {
-        linkedListB = linkedListB.next;
-        lengthB++;
-    }
+        lengthA++
 
-    // Determine the longer and shorter lists
+    }
+    while(linkedListB){
+        linkedListB = linkedListB.next;
+        lengthB++
+    }
     let longerList = lengthA > lengthB ? headA : headB;
     let shorterList = lengthA > lengthB ? headB : headA;
-    let difference = Math.abs(lengthA - lengthB);
+    let difference = Math.max(lengthA, lengthB) - Math.min(lengthA, lengthB)
 
-    // Adjust the longer list's pointer to align with the shorter list
-    while (difference > 0) {
-        longerList = longerList.next;
-        difference--;
+    while(difference > 0){
+            longerList = longerList.next;
+            difference--;
     }
 
-    // Compare nodes to find the intersection
-    while (longerList && shorterList) {
-        if (longerList === shorterList) {
-            return longerList; // Intersection found
-        }
+    while(longerList && shorterList){
+
+        if(longerList === shorterList){
+            return longerList
+        }  
         longerList = longerList.next;
         shorterList = shorterList.next;
-    }
 
-    return null; // No intersection
+    }
+    return null;
 };
